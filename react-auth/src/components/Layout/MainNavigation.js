bec366a31b9594a8489d8ad64ca9c13adebd1c2d
yaroslav bova <yaroslav_bova@epam.com>
2021-09-26 19:33:07 +0300

    finished auth section

--- react-auth/src/components/Layout/MainNavigation.js
+++ react-auth/src/components/Layout/MainNavigation.js
@@ -1,8 +1,23 @@
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+  
+  
+  const isLoggedIn = authCtx.isLoggedIn;
+  
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    //optional: redirect the user
+    history.replace('/auth')
+  }
+
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,15 +25,16 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
+          {!isLoggedIn && <li>
             <Link to='/auth'>Login</Link>
-          </li>
-          <li>
+          </li>}
+         {isLoggedIn && <li>
             <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          </li>}
+
+         {isLoggedIn && <li>
+            <button onClick={logoutHandler} >Logout</button>
+          </li>}
         </ul>
       </nav>
     </header>
